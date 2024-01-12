79c62d2bcf25621604c17fff3060c8ce45e84d24
sotile <sotile@gmail.com>
2024-01-12 09:46:12 -0300

    version 0.0.2

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080
 
 const MODO_PERSISTENCIA = process.env.MODO_PERSISTENCIA || 'MEM'
 const STRCNX = process.env.STRCNX || 'mongodb://127.0.0.1'
-const BASE = process.env.BASE || 'test'
+const BASE = process.env.BASE || 'test' //test
 const FTP_HOST = process.env.FTP_HOST || ''
 const FTP_USER = process.env.FTP_USER || ''
 const FTP_PASS = process.env.FTP_PASS || ''
