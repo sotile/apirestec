de80edd8c0d6096a173e4dea65d28efcf81d0f1b
sotile <sotile@gmail.com>
2024-01-12 15:46:38 -0300

    version 0.0.3

--- controlador/carrito.js
+++ controlador/carrito.js
@@ -6,29 +6,34 @@ class Controlador {
         this.servicio = new Servicio()
     }
 
-    obtenerCarrito = async (req,res) => {
+    obtenerCarrito = async (req, res) => {
         const carrito = await this.servicio.obtenerCarrito()
         res.json(carrito)
     }
 
-    guardarCarrito = async (req,res) => {
+    guardarCarrito = async (req, res) => {
         try {
             const carrito = req.body
             const carritoGuardado = await this.servicio.guardarCarrito(carrito)
             res.json(carritoGuardado)
-        }
-        catch(error) {
-            res.json({ errMsg: error.message })
+        } catch (error) {
+            res.json({
+                errMsg: error.message
+            })
         }
     }
 
-    feedback = async (req,res) => {
+    feedback = async (req, res) => {
         //const info = req.query
         //console.log(info)
 
-        const { payment_id, status, merchant_order_id } = req.query
+        const {
+            payment_id,
+            status,
+            merchant_order_id
+        } = req.query
         console.log(payment_id, status, merchant_order_id)
-        
+
         res.redirect(`http://localhost:3000/carrito?payment_id=${payment_id}&status=${status}&merchant_order_id=${merchant_order_id}`)
     }
 
@@ -37,10 +42,13 @@ class Controlador {
             const prefItems = req.body
             //console.log(prefItems)
             const preferenceId = await this.servicio.createPreference(prefItems)
-            res.json({preferenceId})
-        }
-        catch(error) {
-            res.json({ errMsg: error.message })
+            res.json({
+                preferenceId
+            })
+        } catch (error) {
+            res.json({
+                errMsg: error.message
+            })
         }
     }
 }
